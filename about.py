2a8c2ae6a6228df309a6d2c409ed3d82fa51b0dc
Alex Carrega <contact@alexcarrega.com>
2020-11-28 11:48:55 +0100

    Ident server update

--- about.py
+++ about.py
@@ -1,3 +1,4 @@
+project = 'ASTRID'
 title = 'Context Broker Manager'
 version = '1.0.2'
 description = 'Get and update collected data of the service chain with topology information.'
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ path = os.path.abspath(__file__)
 dir_path = os.path.dirname(path)
 os.chdir(dir_path)
 
-from about import title, version
+from about import project, title, version
 from api import api
 from lib.elasticsearch import connection as es_conn
 from reader.arg import Arg_Reader
@@ -13,7 +13,8 @@ import waitress
 
 db = Arg_Reader.read()
 
-print(f'{title} version:{version}')
+ident = f'{project} - {title} v:{version}'
+print(ident)
 
 if db.version is not None:
     print(db.version)
@@ -21,6 +22,6 @@ else:
     es_conn(endpoint=db.es_endpoint, timeout=db.es_timeout,
             retry_period=db.es_retry_period)
 
-    waitress.serve(api(title=title, version=version,
+    waitress.serve(api(title=title, version=version, ident=ident,
                        dev_username=db.dev_username, dev_password=db.dev_password),
                    host=db.host, port=db.port, expose_tracebacks=False)
