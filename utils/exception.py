dd496296a9d28fd278c9021cfcac757f0b7f2553
Alex Carrega <contact@alexcarrega.com>
2021-03-18 10:46:33 +0100

    Minor changes

--- utils/exception.py
+++ utils/exception.py
@@ -1,8 +1,6 @@
-from pathlib import Path
-from utils.json import loads
-
-import re
+import os
 import sys
+from pathlib import Path
 
 __all__ = [
     'extract_info',
@@ -11,15 +9,21 @@ __all__ = [
 
 
 def extract_info(exception):
-    exc_type, exc_obj, exc_tb = sys.exc_info()
-    filename = exc_tb.tb_frame.f_code.co_filename
     try:
         reason = eval(str(exception))
     except Exception:
-        reason = str(exception) # FIXME if exception is in a JSON format
-    return dict(reason=reason, filename=filename, line=exc_tb.tb_lineno)
+        reason = str(exception)
+    output = dict(reason=reason)
+    exc_type, exc_obj, exc_tb = sys.exc_info()
+    if exc_tb:
+        filename = exc_tb.tb_frame.f_code.co_filename
+        output.update(filename=filename, line=exc_tb.tb_lineno)
+    return output
 
 
 def to_str(exception):
     info = extract_info(exception)
-    return f'{info["reason"]} on filename:{info["filename"]} at line:{info["line"]}'
+    if 'filename' in info and 'line' in info:
+        return f'{info["reason"]} on filename:{info["filename"]} at line:{info["line"]}'
+    else:
+        return info["reason"]
