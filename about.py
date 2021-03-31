430c38475df68d4e89ca88164a1030b9556bbc0b
Alex Carrega <contact@alexcarrega.com>
2021-03-31 11:58:27 +0200

    Minor changes

--- about.py
+++ about.py
@@ -1,4 +1,5 @@
 project = 'ASTRID'
+name = 'cb-manager'
 title = 'Context Broker Manager'
 version = '1.0.4'
 description = 'Get and update collected data of the service chain with topology information.'
